effc0d9fff6aca402ffd52171adadf86e73ff742
Javier Gonzalez <javgon2306@gmail.com>
2024-06-27 21:38:52 -0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+const Suricata = [
+    {
+        "name": "Suricata",
+        "description": "Suricata es un sistema de detección de intrusiones y análisis de logs de seguridad de red. Es un proyecto de código abierto desarrollado por la comunidad de código abierto de Red Hat.",
+        "link": "https://suricata.io/",
+        "img": "https://suricata.io/images/suricata-logo.png",
+    },
+    {
+        "name": "Cerdito",
+        "description": "Suricata es un sistema de detección de intrusiones y análisis de logs de seguridad de red. Es un proyecto de código abierto desarrollado por la comunidad de código abierto de Red Hat.",
+        "link": "https://suricata.io/",
+        "img": "https://suricata.io/images/suricata-logo.png",
+    },
+    {
+        "name": "Aguila",
+        "description": "suricata es un sistema de detección de intrusiones y análisis de logs de seguridad de red. Es un proyecto de código abierto desarrollado por la comunidad de código abierto de Red Hat.",
+        "link": "https://suricata.io/",
+        "img": "https://suricata.io/images/suricata-logo.png",
+    }
+]
